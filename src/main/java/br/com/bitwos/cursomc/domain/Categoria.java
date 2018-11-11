cd62a2bb1189edb55712bcd18253ed1337cee160
Samuel Ferreira Duarte <samuel_fx@hotmail.com>
2018-11-11 15:24:52 -0200

    Testando a primeira classe de dominio

---
+++ src/main/java/br/com/bitwos/cursomc/domain/Categoria.java
@@ -0,0 +1,75 @@
+package br.com.bitwos.cursomc.domain;
+
+import java.io.Serializable;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Categoria(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nome=" + nome + "]";
+	}
+	
+}
--- src/main/java/br/com/bitwos/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/bitwos/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package br.com.bitwos.cursomc.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.bitwos.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@GetMapping
-	public String listar() {
-		return "Rest está funcionando";
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria("Informatica");
+		Categoria cat2 = new Categoria("Escritorio");
+		
+		List<Categoria> listaCategoria = new ArrayList<>();
+		
+		listaCategoria.add(cat);
+		listaCategoria.add(cat2);
+		
+		return listaCategoria;
 	}
 	
 }
