132a9034571e29a2d541539b0a016b94f3d9f1b4
Samuel Ferreira Duarte <samuel_fx@hotmail.com>
2018-11-15 07:37:08 -0200

    Criando operação de instanciação

--- src/main/java/br/com/bitwos/cursomc/CursomcApplication.java
+++ src/main/java/br/com/bitwos/cursomc/CursomcApplication.java
@@ -1,12 +1,28 @@
 package br.com.bitwos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.bitwos.cursomc.domain.Categoria;
+import br.com.bitwos.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	private @Autowired CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria("Informática");
+		Categoria cat2 = new Categoria("Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
