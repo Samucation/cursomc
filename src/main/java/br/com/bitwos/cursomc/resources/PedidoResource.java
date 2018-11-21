0e0dc34e2e64ac0dc4baf34d4e3b2acfcadc5200
BITwos IT <bitwos.it@gmail.com>
2018-11-21 16:36:13 -0200

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/bitwos/cursomc/resources/PedidoResource.java
@@ -0,0 +1,25 @@
+package br.com.bitwos.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.bitwos.cursomc.domain.Pedido;
+import br.com.bitwos.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	private @Autowired PedidoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido cat = service.buscar(id);
+		return ResponseEntity.ok().body(cat);
+	}
+	
+}
