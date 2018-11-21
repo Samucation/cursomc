4d967f1d7235ae96c2a1ed4428cddcec80200072
BITwos IT <bitwos.it@gmail.com>
2018-11-21 16:51:18 -0200

    Atualização: utilizando somento jsonIgnore

--- src/main/java/br/com/bitwos/cursomc/domain/Pedido.java
+++ src/main/java/br/com/bitwos/cursomc/domain/Pedido.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable{
@@ -30,11 +29,9 @@ public class Pedido implements Serializable{
 	@JsonFormat(pattern="dd/MM/yyyy HH:mm:ss")
 	private Date instante;
 	
-	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
@@ -100,6 +97,22 @@ public class Pedido implements Serializable{
 		this.pagamento = pagamento;
 	}
 	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Endereco getEnderecoDeEntrega() {
+		return enderecoDeEntrega;
+	}
+
+	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
 	public Set<ItemPedido> getItens() {
 		return itens;
 	}
